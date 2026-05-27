351b63ea57c84086f03f45276a0b038228457fcb
Robert Aldridge <bamarob55@gmail.com>
2026-05-27 11:13:59 -0500

    Update views.py

--- hunt_logs/views.py
+++ hunt_logs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
+from django.utils.dateparse import parse_datetime
 from .models import HuntLog
 from .models import Harvest
 import folium
