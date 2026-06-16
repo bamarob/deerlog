d008ed9ab34ca2ed0488a733753cb3d9215e4255
Robert Aldridge <bamarob55@gmail.com>
2026-06-16 13:49:17 +0000

    making sure everything is tracked in git

--- core/settings.py
+++ core/settings.py
@@ -116,9 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/BamaRob55/deerlog/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'dashboard'
--- core/urls.py
+++ core/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.bind if hasattr(admin.site, 'bind') else admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='hunt_logs/login.html'), name='login'),
     path('hunts/', include('hunt_logs.urls')), #Mounts our custom logging pages under /hunts/
     path('', RedirectView.as_view(url='/hunts/dashboard/', permanent=True))
 ]
